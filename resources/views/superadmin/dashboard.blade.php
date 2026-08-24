0f8a8c82e2f54f0ccb850f0d51c24bc0f1029b95
brahangdale <bhartiedu007@gmail.com>
2026-08-24 13:29:37 +0530

    view on dashboard

--- resources/views/superadmin/dashboard.blade.php
+++ resources/views/superadmin/dashboard.blade.php
@@ -88,7 +88,10 @@
             <td>{{ $clinic->created_at->format('d-M-Y') }}</td>
 
             <td>
-              <button class="btn btn-sm btn-primary">View</button>
+              <!-- <button class="btn btn-sm btn-primary">View</button> -->
+               <button class="btn btn-sm btn-primary" data-bs-toggle="offcanvas" data-bs-target="#samClinicViewOffcanvas{{ $clinic->id }}">
+                        View
+                      </button>
             </td>
           </tr>
           @empty
@@ -100,6 +103,136 @@
       </table>
     </div>
   </div>
+
+  @foreach($clinics as $clinic)  
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="samClinicViewOffcanvas{{ $clinic->id }}">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title">Clinic Details</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
+        </div>
+        <div class="offcanvas-body">
+          <div class="sam-clinic-view-section">
+            <h6>Clinic Information</h6>
+            <table class="table">
+              <tr>
+                <th>Clinic Name</th>
+                <td>{{$clinic->clinic_name}}</td>
+              </tr>
+
+              <tr>
+                  <th>Owner</th>
+                  <td>{{$clinic->user?->user_name}}</td>
+              </tr>
+
+              <tr>
+                  <th>Mobile</th>
+                  <td>{{$clinic->mobile_number}}</td>
+              </tr>
+
+              <tr>
+                  <th>Email</th>
+                  <td>{{ $clinic->user?->email }}</td>
+              </tr>
+
+              <tr>
+                  <th>Status</th>
+                  <td>
+                    @if($clinic->user?->status == 1)
+                      <span class="badge bg-success">Active</span>
+                    @else
+                      <span class="badge bg-danger">Inactive</span>
+                    @endif
+                  </td>
+              </tr>
+            </table>
+          </div>
+          <hr>
+          <div class="sam-clinic-view-section">
+
+            <h6>Admin Credentials</h6>
+
+            <table class="table">
+
+                <tr>
+                    <th>Username</th>
+                    <td>{{$clinic->user?->user_name}}</td>
+                </tr>
+
+                <tr>
+                    <th>Password</th>
+                    <td>{{$clinic->user?->password}}</td>
+                </tr>
+
+                <tr>
+                    <th>Admin URL</th>
+                    <td>yourdomain.com/admin</td>
+                </tr>
+
+                <tr>
+                    <th>Patient URL</th>
+                    <td>yourdomain.com/akasa-clinic</td>
+                </tr>
+
+            </table>
+
+          </div>
+
+          <hr>
+
+          <div class="row g-3">
+
+                <div class="col-6">
+
+                    <div class="sam-clinic-mini-card">
+
+                        <h6>Doctors</h6>
+
+                        <h3>{{ $clinic->doctors_count }}</h3>
+
+                    </div>
+
+                </div>
+
+                <div class="col-6">
+
+                    <div class="sam-clinic-mini-card">
+
+                        <h6>Patients</h6>
+
+                        <h3>{{ $clinic->appointments_count }}</h3>
+
+                    </div>
+
+                </div>
+
+                <div class="col-6">
+
+                    <div class="sam-clinic-mini-card">
+
+                        <h6>Appointments</h6>
+
+                        <h3>{{ $clinic->appointments_count }}</h3>
+
+                    </div>
+
+                </div>
+
+                <!-- <div class="col-6">
+
+                    <div class="sam-clinic-mini-card">
+
+                        <h6>Receptionists</h6>
+
+                        <h3>4</h3>
+
+                    </div>
+
+                </div> -->
+
+          </div>
+        </div>
+      </div>
+      @endforeach
 </div>
 
 <!-- </div> -->
